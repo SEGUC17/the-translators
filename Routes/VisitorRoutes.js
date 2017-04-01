c5fc0009e38dadd390adf4dfd95ecf7097309f0e
MariamShalaby <mariamshalaby95@gmail.com>
2017-04-01 10:52:44 +0200

    viewpage

--- Routes/VisitorRoutes.js
+++ Routes/VisitorRoutes.js
@@ -6,4 +6,43 @@ router.get('/', function(req, res){
   res.console('this page');
 });
 
+app.post(‘/api/photo’,function(req,res){
+ var newItem = new VisitorSchema();
+ newItem.img.data = fs.readFileSync(req.files.userPhoto.path)
+ newItem.img.contentType = ‘image/png’;
+ newItem.save();
+});
+
+router.get('/VisitorView', function(request, response){
+	response.render('VisitorView');
+})
+
+
+router.post('/VisitorView', function(request, response){
+	var gymname = request.body.gymname;
+	var gymdescription = request.body.gymdescription;
+	var gymproduct = request.body.gymproduct;
+  var gymrating = request.body.gymrating;
+  var gymimgage = request.body.gymimgage;
+  var gymschedule = request.body.gymschedule;
+  var gymreview = request.body.gymreview;
+	db_collection.find({Gymname:gymname}).toArray(function(err,result){
+	 if(err){
+		 throw err;
+	 }
+	 else if(result.length)
+	 {
+		 response.render('VisitorView'); // selected page
+}else {
+	console.log("no document found");
+	response.render('GeneralView'); // homepage
+}
+	});
+
+}); // selected gym page
+
+
+
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var Crouter = require('./Routes/CustomerRoutes.js');
 var Vrouter = require('./Routes/VisitorRoutes.js');
 var DB_URI = "mongodb://localhost:27017/GymPlatform";
 
+var Schema = mongoose.Schema;
+var multer = require('multer');
 var app = express();
 
 app.use(bodyParser.json());
@@ -19,6 +21,36 @@ app.use(Brouter);
 app.use(Crouter);
 app.use(Vrouter);
 
+app.use(multer({ dest: ‘./uploads/’,
+ rename: function (fieldname, filename) {
+   return filename;
+ },
+})); //add the multer so that to destinguish the path of the gym/schedule photos
+
+/*MongoClient.connect(DB_URI,function(err,db)
+{
+  if(err){
+  throw err;
+}
+  else {
+    console.log("Mongo db Connected");
+  }
+  var collection = db.collection('Customer');
+  db_collection = collection;
+  collection.find({}).toArray(function(err,result){
+   if(err){
+     throw err;;
+   }
+   else if(result.length)
+   {
+     module.exports = db;
+     console.log("did it");
+}else {
+  console.log("no document found");
+}
+  });
+});
+*/
 app.listen(8080, function(){
   console.log("server is listening on port 8080");
 })
