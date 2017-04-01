f9b07c76bb906f55ac9dd1ec293ab1d006d6750e
benyamin el kess ibraam thomas labib <benyamin@benyamins-MacBook-Pro.local>
2017-04-01 12:49:15 +0200

    subscribe

---
+++ Models/BusinessModel.js
@@ -0,0 +1,38 @@
+var mongoose= require ('mongoose');
+var gymSchema = mongoose.Schema({
+    gym_name:
+        {type: String, required: true},
+    username: String,
+    location:
+        {type: String, required:true},
+    Email:
+        {type: String, required:true, unique:true},
+    password:
+        {type: String, required:true},
+    // confirm_password: maynfa3shy tkooon maogooda
+    //     {type:String, required:true},
+    schedules:
+        {type:JPEG, required:true},//IMAGE
+    //price:
+    //    {type:String, required:true},
+    //product_name:
+    //    {type:String},
+    //product_description:
+    //    {type:String},
+    Gym_description:
+        {type:String, required:true}
+});
+
+var gym=  mongoose.model("Gym", gymSchema);
+module.exports= gym;
+
+var mongoose= require('mongoose');
+var uploadscheduleSchema= mongoose.Schema({
+//Business username and schedule of all timings
+business_username:
+{type: String, required: true, unique: true},
+schedule:
+{type: String, required:true, unique: true},
+});
+var uploadschedule= mongoose.model("upload schedule",uploadschedule);
+module.exports= uploadschedule;
