86a7df66d5e341d0563bc1de46dafc3bcb4e92d4
benyamin el kess ibraam thomas labib <benyamin@benyamins-MacBook-Pro.local>
2017-04-03 15:45:19 +0200

    Business Model

--- Models/BusinessModel.js
+++ Models/BusinessModel.js
@@ -1,26 +1,35 @@
 var mongoose= require ('mongoose');
 var gymSchema = mongoose.Schema({
-    gym_name:
-        {type: String, required: true},
-    username: String,
-    location:
+    gymname-location:
+        {type: String, required: true, unique:true},
+//username: String,
+    address:
         {type: String, required:true},
     Email:
-        {type: String, required:true, unique:true},
+        {type: String, required:true,unique:true},
+    username:
+        {type:String,required:true,unique:true},
     password:
         {type: String, required:true},
     // confirm_password: maynfa3shy tkooon maogooda
     //     {type:String, required:true},
+    description:
+        {type:String},
+    phone number:
+        {type:Number,required:true},
+    Bank name:
+        {type:String,required:true},
+    Account number:
+        {type:String,required:true},
     schedules:
-        {type:JPEG, required:true},//IMAGE
+        {type:File},//IMAGE
     //price:
     //    {type:String, required:true},
     //product_name:
     //    {type:String},
     //product_description:
     //    {type:String},
-    Gym_description:
-        {type:String, required:true}
+
 });
 
 var gym=  mongoose.model("Gym", gymSchema);
