aa6287adf7a156d1539108b98b4219feafbff23f
Mina Ramsis <mina_wamsis@hotmail.com>
2017-04-06 15:00:23 +0200

    Added c/b View Profile Routes
    - Added a route to customer view profile in CustomerRoutes.
    - Added a route to business view profile in BusinessRoutes.

--- Routes/BusinessRoutes.js
+++ Routes/BusinessRoutes.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var BusinessController = require('../Controllers/BusinessController.js');
+
 var router = express.Router();
 
 router.get('/', function(req, res){
   res.send('this page');
 });
+
+// route to business profile page
+router.get('/profile', BusinessController.getBusiness);
 
 module.exports = router;
--- Routes/CustomerRoutes.js
+++ Routes/CustomerRoutes.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var CustomerController = require('../Controllers/CustomerController.js');
 
 var router = express.Router();
 
 router.get('/', function(req, res){
   res.send('this page');
 });
+
+// route to customer profile page
+router.get('/profile', CustomerController.getCustomer);
 
 module.exports = router;
