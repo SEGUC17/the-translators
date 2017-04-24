a7ccee97842b1a32821d0f9ce19a8803e06f6f9d
hannaMaamoun <hanna.maamoun@hotmail.com>
2017-04-24 23:54:31 +0200

    Add files via upload

--- angular-src/src/app/services/bookingvalidation.service.ts
+++ angular-src/src/app/services/bookingvalidation.service.ts
@@ -5,8 +5,10 @@ export class BookingvalidationService {
 
   constructor() { }
 
-  validateBooking(booking){
-    if( booking.User_ID == undefined || booking.CustomerUsername == undefined || booking.GymNameLocation == undefined || booking.selectedClass == undefined || booking.selectedTime == undefined || booking.CustomerEmail == undefined || booking.CustomerMobile == undefined || booking.ConfirmationStatus == undefined){
+  validateBooking(bookingrequest){
+    if( bookingrequest.CustomerUsername == undefined || bookingrequest.GymNameLocation == undefined ||
+      bookingrequest.selectedClass == undefined || bookingrequest.selectedTime == undefined ||
+      bookingrequest.selectedDate ==undefined ){
       return false;
     } else {
       return true;
