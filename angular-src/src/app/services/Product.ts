585c66a61789b46d13fc82585a36540c26909a01
NorhanHamdy <norhanh.abdelhalim@yahoo.com>
2017-04-25 00:13:16 +0200

    Add files via upload

---
+++ angular-src/src/app/services/Product.ts
@@ -0,0 +1,10 @@
+export class Product {
+  businessUserName: String;
+  prodname: String;
+  prodID: number;
+  price: number;
+  image: String;
+  ProductDescription: String;
+  Category: String;
+  Quantity: number;
+}
