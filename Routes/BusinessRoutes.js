44f556225a799af241b79d2e11672934c2ae6d66
hannaMaamoun <hanna.maamoun@hotmail.com>
2017-04-24 23:43:38 +0200

    Update BusinessRoutes.js

--- Routes/BusinessRoutes.js
+++ Routes/BusinessRoutes.js
@@ -2,17 +2,25 @@
 var express = require("express");
 var router = express.Router();
 
-var BusinessController = require("../Controllers/BusinessController");
+var BookingController = require('../Controllers/BookingController');
 
-//var Business= require("../Models/BusinessModel");
-router.post("/viewproducts", BusinessController.viewproducts);
+var BusinessController = require('../Controllers/BusinessController');
+var Business= require('../Models/BusinessModel');
 
 router.post("/updatedgymschedule", BusinessController.uploadGymSchedule);
 
-router.post("/editproducts", BusinessController.editproducts);
 
-router.post("/removeproducts", BusinessController.removeproducts);
+router.get("/viewproducts", BusinessController.viewProducts);
+router.put("/updateproducts", BusinessController.updateProduct);
+router.delete("/removeproduct", BusinessController.removeProduct);
+
 
 router.post("/uploadproducts", BusinessController.createproduct);
+
+//Business Owner views Customers' booking requests
+router.get('/BusinessBookings', BookingController.ViewBusinessBookingList); //double check if i can use the functionality in router.get or not!
+//Business Owner changed the booking status of a specific booking requests
+router.post('/ChangeBookingStatus', BookingController.ChangeCustomerBookingRequestStatus); //double check the path used if it is convenient since the business owner shouldn't be redirected to a new page when editing the booking status
+
 
 module.exports = router;
