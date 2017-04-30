da6365a02e4d1b6fd64ba0ea90ca4e778f7b47e3
SalmaAkram <salma.badawy@student.guc.edu.eg>
2017-04-30 06:11:16 +0200

    Sprint3

---
+++ public/js/controllers/LoginController.js
@@ -0,0 +1,24 @@
+var LoginController = function($scope, $location, factory, $http){
+    $scope.Username = '';
+    $scope.Password= '';
+    $scope.OnSubmitLogin = function(){
+        var User = {
+            username: $scope.Username,
+            password: $scope.Password
+        }
+        
+    $http.post('http://localhost:8080/customerlogin', User)
+    .then(function(data){
+        factory.SetUser(User);
+        alert(data.data.data);
+        console.log(data.data.data);
+        $location.path("/HomePage"); //create the html file for the logged in user homepage
+    }).catch(function(error){
+        console.log(error.data.error);
+        alert(error.data.error);
+    });
+}
+}
+
+LoginController.$inject =['$scope', '$location', 'factory', '$http'];
+App.controller('LoginController',LoginController);
